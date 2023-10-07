b8b3e221458b52fcc79d4b600332d7a88c973753
scragonrex <scragondragon@gmail.com>
2023-10-07 10:21:37 +0530

    added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,4 +2,4 @@ import express from "express";
 import {login} from "../controller/auth.js";
 const router = express.Router();
 router.post("/login",login);
-export default router; 
+export default router;  
